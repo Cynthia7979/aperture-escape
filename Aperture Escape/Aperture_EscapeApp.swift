d7077a001231f68a77ef75963a566589c400da50
Cynthia Wang <CynthiaWang7979@hotmail.com>
2021-07-24 02:10:02 +0800

    Initial Commit

---
+++ Aperture Escape/Aperture_EscapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aperture_EscapeApp.swift
+//  Aperture Escape
+//
+//  Created by Xia He on 2021/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Aperture_EscapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aperture EscapeTests/Aperture_EscapeTests.swift
@@ -0,0 +1,33 @@
+//
+//  Aperture_EscapeTests.swift
+//  Aperture EscapeTests
+//
+//  Created by Xia He on 2021/7/24.
+//
+
+import XCTest
+@testable import Aperture_Escape
+
+class Aperture_EscapeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
